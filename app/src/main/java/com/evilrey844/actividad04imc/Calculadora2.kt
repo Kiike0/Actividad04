221ee476b39bbcdc6855a722c6072a5fc8a9ee6d
Kike <enriquevilrey@gmail.com>
2023-10-25 16:08:07 +0200

    Calculadora 2 de Agus añadida

--- app/src/main/java/com/evilrey844/actividad04imc/Calculadora2.kt
+++ app/src/main/java/com/evilrey844/actividad04imc/Calculadora2.kt
@@ -2,10 +2,100 @@ package com.evilrey844.actividad04imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+/**
+ * Clase MainActivity que hereda de AppCompatActivity.
+ * Esta clase se encarga de manejar la interfaz de usuario de la aplicación.
+ *
+ * @property calculo Instancia de la clase Calculo que se encarga de realizar los cálculos.
+ * @property txtResultado TextView que muestra el resultado de los cálculos.
+ */
+@Suppress("SpellCheckingInspection")
 class Calculadora2 : AppCompatActivity() {
+    private lateinit var calculo: Calculo2
+    private lateinit var txtResultado: TextView
+
+
+    // Declaro botones numéricos y operacionales que usaré asociados a su id correspondiente.
+    private val botonesNumeros = listOf(
+        R.id.boton0, R.id.boton1, R.id.boton2, R.id.boton3,
+        R.id.boton4, R.id.boton5, R.id.boton6, R.id.boton7,
+        R.id.boton8, R.id.boton9
+    )
+
+    private val botonesOperaciones = listOf(
+        R.id.botonSuma, R.id.botonResta,
+        R.id.botonMultiplicacion, R.id.botonDivision
+    )
+
+    private fun actualizarTextoResultado() {
+        txtResultado.text = getString(R.string.resultado_texto, calculo.num1, calculo.operacion, calculo.num2)
+    }
+    /**
+     * Método onCreate que se ejecuta cuando se crea la actividad
+     *
+     * @param savedInstanceState Bundle que contiene el estado guardado de la actividad.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora2)
+
+        calculo = Calculo2(this)
+        txtResultado = findViewById(R.id.textViewResultado)
+
+        // Bucle for para asignar la funcionalidad a los botones numéricos
+        for (id in botonesNumeros) {
+            val boton = findViewById<Button>(id)
+            boton.setOnClickListener {
+                calculo.setNumClicked((it as Button).text.toString())
+                txtResultado.text = if (calculo.operacion.isEmpty()) calculo.num1 else calculo.num2
+                actualizarTextoResultado()
+            }
+        }
+
+        // Bucle for para asignar la funcionalidad a los botones operacionales.
+        for (id in botonesOperaciones) {
+            val boton = findViewById<Button>(id)
+            boton.setOnClickListener {
+                calculo.setOperaciones((it as Button).text.toString())
+                actualizarTextoResultado()
+            }
+        }
+
+        // Trabajo con el botón igual, llamo a la función igual. Añado un condicional que me permite discriminar
+        // el decimal del número si este es "0".
+        val botonIgual = findViewById<Button>(R.id.botonIgual)
+        botonIgual.setOnClickListener {
+            calculo.calcular()
+            // Compruebo si el decimal se puede descartar para convertirlo a entero y que no se muestre el 0
+            if (calculo.resultado % 1 == 0.0) {
+                txtResultado.text = calculo.resultado.toInt().toString()
+            } else { // Si el número no tiene resto "0", respeto el float y lo convierto a string
+                txtResultado.text = calculo.resultado.toString()
+            }
+        }
+
+        val botonPunto = findViewById<Button>(R.id.botonPunto)
+        botonPunto.setOnClickListener {
+            calculo.setPuntoClicked()
+            actualizarTextoResultado()
+        }
+
+        // Botón CE (Clear Entry) para resetear los valores en la calculadora y en el TextView del resultado.
+        val botonCE = findViewById<Button>(R.id.botonCE)
+        botonCE.setOnClickListener {
+            calculo.resetear()
+            txtResultado.text = ""
+        }
+
+        // Botón Limpiar C para eliminar dígitos y operadores. Añado recurso a mi string.xml para
+        // mejorar la accesibilidad y una futurible localización de mi código.
+        val botonLimpiarC = findViewById<Button>(R.id.botonLimpiarC)
+        botonLimpiarC.setOnClickListener {
+            calculo.botonLimpiarC()
+            txtResultado.text = getString(R.string.resultado_texto, calculo.num1, calculo.operacion, calculo.num2)
+        }
     }
 }
--- app/src/main/java/com/evilrey844/actividad04imc/Calculo2.kt
+++ app/src/main/java/com/evilrey844/actividad04imc/Calculo2.kt
@@ -1,4 +1,135 @@
 package com.evilrey844.actividad04imc
 
-class Calculo2 {
+import android.content.Context
+import android.widget.Toast
+
+class Calculo2 (private val context: Context) {
+    var num1: String = ""
+    var num2: String = ""
+    var operacion: String = ""
+    var resultado: Double = 0.0
+    var fase: Int = 0 // Controla la fase en la que está mi calculadora (de 0 a 3).
+    var operacionRecienIntroducida = false
+
+    /**
+     * Los siguientes funciones se encargan de crear la lógica operacional de la calculadora.
+     */
+    fun sumar() {
+        resultado = num1.toDouble() + num2.toDouble()
+    }
+
+    fun restar() {
+        resultado = num1.toDouble() - num2.toDouble()
+    }
+
+    fun multiplicar() {
+        resultado = num1.toDouble() * num2.toDouble()
+    }
+
+    fun dividir() {
+        resultado = num1.toDouble() / num2.toDouble()
+    }
+
+    fun mostrarToast(mensaje: String) {
+        Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * La función calcular maneja la situación en la que se dispara el Toast. Además, euipara los símbolos Strings
+     * de operaciones a los métodos de sumar, restar, multiplicar y dividir. Añado un condicional para comprobar que
+     * el decimal sea 0, si es 0, convierto el número a entero antes de convertirlo a String. Por otra parte, al haberse
+     * pulsado el operador, la calculadora pasa a fase 3 (Inserte num2).
+     */
+    fun calcular() {
+        if (num1.isEmpty() || num2.isEmpty() || operacion.isEmpty()) { // Llamo al toast si pulso igual y no hay nada.
+            mostrarToast("Debe introducir 2 números y una operación para mostrar un resultado.")
+            return
+        }
+        when (operacion) {
+            "+" -> sumar()
+            "-" -> restar()
+            "x" -> multiplicar()
+            "/" -> dividir()
+        }
+        // Compruebo si el resultado es un número entero para descartar decimales "0" y mostrar el resultado bonito.
+        if (resultado % 1 == 0.0) {
+            num1 = resultado.toInt().toString()
+        } else {
+            num1 = resultado.toString()
+        }
+        num2 = ""
+        operacion = ""
+        fase =
+            3 // Se ha realizado una operación, si se presiona un número la calculadora se resetea.
+    }
+
+    /**
+     * Si calculadora está en fase 0, 2, 3, lleva a cabo las siguientes tareas.
+     */
+    fun setNumClicked(num: String) {
+        if (fase == 0) {
+            num1 += num
+        } else if (fase == 2) {
+            num2 += num
+        } else if (fase == 3) {
+            resetear()
+            num1 += num
+            fase = 0 // Esperando a num1
+        }
+    }
+
+    /**
+     * Comprueba que num1 no esté vacio, Si es así, la calculadora pasa
+     * a fase2, espera a que inserte el botón de operación "/" "-" "x" "+"
+     */
+    fun setOperaciones(oper: String) {
+        if (num1.isNotEmpty()) {
+            if (operacion.isNotEmpty() && num2.isNotEmpty()) {
+                calcular()
+            }
+            operacion = oper
+            fase = 2 // Esperando a num2
+        }
+    }
+
+    /**
+     * Todas las variables quedan vacias cuando se llama a esta función. Calculadora pasa a fase 0,
+     * (espera a num1). Llamo a este método cuando quiero resetear.
+     */
+    fun resetear() {
+        num1 = ""
+        num2 = ""
+        operacion = ""
+        resultado = 0.0
+        fase = 0
+        operacionRecienIntroducida = false
+    }
+
+    fun setPuntoClicked() {
+        if (fase == 0 && !num1.contains(".")){
+            num1 += "."
+        } else if (fase == 2 && !num2.contains(".")) {
+            num2 += "."
+        }
+    }
+
+    /**
+     * Método `botonLimpiarC` que se llama cuando se pulsa el botón C.
+     * Borra los dígitos uno a uno del número actual.
+     * Si no hay más dígitos para borrar, borra la operación.
+     * Si no hay operación para borrar, borra los dígitos del primer número.
+     * Si no hay nada que eliminar, se emite un mensaje tipo toast "No hay nada más que borrar".
+     */
+    fun botonLimpiarC() {
+        if (num2.isNotEmpty()) {
+            num2 = num2.dropLast(1)
+        } else if (operacion.isNotEmpty()) {
+            operacion = ""
+            fase = 0 // Esperando a num1
+        } else if (num1.isNotEmpty()) {
+            num1 = num1.dropLast(1)
+        } else {
+            mostrarToast("No hay nada más que borrar.")
+        }
+    }
 }
